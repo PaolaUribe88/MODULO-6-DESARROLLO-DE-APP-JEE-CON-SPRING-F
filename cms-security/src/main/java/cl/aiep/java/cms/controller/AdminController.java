7c577ad48292e808ca5b0fb30cfa82fb7ab2ef53
Paola.Uribe <paolauribe88@gmail.com>
2022-05-19 22:31:21 -0400

    Autenticacion sin BD externa, Autorizacion

---
+++ cms-security/src/main/java/cl/aiep/java/cms/controller/AdminController.java
@@ -0,0 +1,19 @@
+package cl.aiep.java.cms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+	
+	@GetMapping("/index")
+	public String index() {
+		return "admin/index";
+	}
+	@GetMapping("/reporte")
+	public String reporte() {
+		return "admin/reporte";
+	}
+}
